a60d5913fe18aff4cd2cd2b381af5c2f22fdd9ac
devel0624 <devel0624@gmail.com>
2024-12-11 18:06:39 +0900

    initialize repository commit

--- src/main/java/com/nhnacademy/enviroment/EnvironmentApplication.java
+++ src/main/java/com/nhnacademy/enviroment/EnvironmentApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EnviromentApplication {
+public class EnvironmentApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(EnviromentApplication.class, args);
+        SpringApplication.run(EnvironmentApplication.class, args);
     }
 
 }
--- src/test/java/com/nhnacademy/enviroment/EnvironmentApplicationTests.java
+++ src/test/java/com/nhnacademy/enviroment/EnvironmentApplicationTests.java
@@ -4,10 +4,11 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class EnviromentApplicationTests {
+class EnvironmentApplicationTests {
 
     @Test
     void contextLoads() {
+
     }
 
 }
