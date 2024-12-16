991163c95e50fe20ce6f894114d8bbb9741d766e
devel0624 <devel0624@gmail.com>
2024-12-16 11:42:29 +0900

    enable logNcrash

--- src/main/java/com/nhnacademy/enviroment/EnvironmentApplication.java
+++ src/main/java/com/nhnacademy/enviroment/EnvironmentApplication.java
@@ -1,12 +1,15 @@
 package com.nhnacademy.enviroment;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class EnvironmentApplication {
 
     public static void main(String[] args) {
+        log.error("Starting Environment Application");
         SpringApplication.run(EnvironmentApplication.class, args);
     }
 
